6a8c25e0b09f2089f01a2c510b35aa5bbf2b5661
Nathanael Alain <nathanaelalain@yahoo.fr>
2019-05-15 13:46:15 +0200

    Rails new with frontend and form gems

---
+++ db/migrate/20190515105827_create_restaurants.rb
@@ -0,0 +1,12 @@
+class CreateRestaurants < ActiveRecord::Migration[5.2]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :phone
+      t.text :category
+      t.text :reviews
+
+      t.timestamps
+    end
+  end
+end
